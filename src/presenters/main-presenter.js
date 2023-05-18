39471a5d8f44422a54d3b4616ff112a96c54b0ef
AricsyGIT <aricsybsn@gmail.com>
2023-05-18 16:14:54 +0500

    отрефакторил код

--- src/presenters/main-presenter.js
+++ src/presenters/main-presenter.js
@@ -2,14 +2,13 @@ import { render } from '../render.js';
 import EditPointView from '../view/edit-point-view';
 import PointView from '../view/point-view';
 import SortView from '../view/sort-view';
-import NoPointsView from "../view/no-points-view";
+import NoPointsView from '../view/no-points-view';
 
 export default class MainPresenter {
   #containerListElement = null;
   #points = null;
   #destinations = null;
   #offers = null;
-
 
   constructor(containerListElement) {
     this.#containerListElement = containerListElement;
@@ -21,11 +20,10 @@ export default class MainPresenter {
     this.#destinations = destinationsData;
     const tripSectionElement = document.querySelector('.trip-events');
     if (this.#points.length === 0) {
-      render(new NoPointsView(), tripSectionElement)
+      render(new NoPointsView(), tripSectionElement);
     } else {
       render(new SortView(), tripSectionElement);
       render(this.#containerListElement, tripSectionElement);
-
 
       for (const point of this.#points) {
         this.#renderPoint(point);
@@ -34,15 +32,29 @@ export default class MainPresenter {
   }
 
   #renderPoint = (point) => {
-    const pointComponent = new PointView(point, this.#offers, this.#destinations);
-    const pointEditComponent = new EditPointView(point, this.#offers, this.#destinations);
+    const pointComponent = new PointView(
+      point,
+      this.#offers,
+      this.#destinations
+    );
+    const pointEditComponent = new EditPointView(
+      point,
+      this.#offers,
+      this.#destinations
+    );
 
     const replacePointToEditForm = () => {
-      this.#containerListElement.element.replaceChild(pointEditComponent.element, pointComponent.element);
+      this.#containerListElement.element.replaceChild(
+        pointEditComponent.element,
+        pointComponent.element
+      );
     };
 
     const replaceEditFormToPoint = () => {
-      this.#containerListElement.element.replaceChild(pointComponent.element, pointEditComponent.element);
+      this.#containerListElement.element.replaceChild(
+        pointComponent.element,
+        pointEditComponent.element
+      );
     };
 
     const onEscKeyDown = (e) => {
@@ -53,23 +65,29 @@ export default class MainPresenter {
       }
     };
 
-    pointComponent.element.querySelector('.event__rollup-btn').addEventListener('click', (e) => {
-      e.preventDefault();
-      replacePointToEditForm();
-      document.addEventListener('keydown', onEscKeyDown);
-    });
+    pointComponent.element
+      .querySelector('.event__rollup-btn')
+      .addEventListener('click', (e) => {
+        e.preventDefault();
+        replacePointToEditForm();
+        document.addEventListener('keydown', onEscKeyDown);
+      });
 
-    pointEditComponent.element.querySelector('.event__rollup-btn').addEventListener('click', (e) => {
-      e.preventDefault();
-      replaceEditFormToPoint();
-      document.removeEventListener('keydown', onEscKeyDown);
-    });
+    pointEditComponent.element
+      .querySelector('.event__rollup-btn')
+      .addEventListener('click', (e) => {
+        e.preventDefault();
+        replaceEditFormToPoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      });
 
-    pointEditComponent.element.querySelector('form').addEventListener('submit', (e) => {
-      e.preventDefault();
-      replaceEditFormToPoint();
-      document.removeEventListener('keydown', onEscKeyDown);
-    });
+    pointEditComponent.element
+      .querySelector('form')
+      .addEventListener('submit', (e) => {
+        e.preventDefault();
+        replaceEditFormToPoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      });
 
     render(pointComponent, this.#containerListElement.element);
   };
