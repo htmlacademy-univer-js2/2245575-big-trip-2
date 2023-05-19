0734d288760299d8555b2ec67be44bf9f2b88086
AricsyGIT <aricsybsn@gmail.com>
2023-05-19 17:51:58 +0500

    отрефакторил код

--- src/view/edit-form-view.js
+++ src/view/edit-form-view.js
@@ -21,14 +21,8 @@ const createAvailableOptionsTemplate = (offers, eventType) => {
     .map(
       (offer) =>
         `<div class="event__offer-selector">
-      <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer.title
-        .split(' ')
-        .pop()}-${offer.id}" type="checkbox" name="event-offer-${offer.title
-          .split(' ')
-          .pop()}">
-      <label class="event__offer-label" for="event-offer-${offer.title
-        .split(' ')
-        .pop()}-${offer.id}">
+      <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer.title.split(' ').pop()}-${offer.id}" type="checkbox" name="event-offer-${offer.title.split(' ').pop()}">
+      <label class="event__offer-label" for="event-offer-${offer.title.split(' ').pop()}-${offer.id}">
         <span class="event__offer-title">${offer.title}</span>
         &plus;&euro;&nbsp;
         <span class="event__offer-price">${offer.price}</span>
@@ -44,13 +38,7 @@ const createDestinationDescriptionTemplate = (destinations, name) =>
 const createPicturesListTemplate = (pictures) =>
   `<div class="event__photos-container">
       <div class="event__photos-tape">
-      ${pictures.reduce(
-        (result, picture) =>
-          result.concat(
-            `<img class="event__photo" src="${picture.src}" alt="Event photo">`
-          ),
-        ''
-      )}
+      ${pictures.reduce((result, picture) => result.concat(`<img class="event__photo" src="${picture.src}" alt="Event photo">`),'')}
       </div>
    </div>`;
 
@@ -112,23 +100,17 @@ const createEditFormTemplate = (event) => {
                 <label class="event__label  event__type-output" for="event-destination-1">
                   ${capitalizeFirstLetter(type)}
                 </label>
-                <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${
-                  data.name
-                }" list="destination-list-1">
+                <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${data.name}" list="destination-list-1">
                 <datalist id="destination-list-1">
                   ${createDestionationsOptionsTemplate(DESTINATIONS)}
                 </datalist>
               </div>
               <div class="event__field-group  event__field-group--time">
                 <label class="visually-hidden" for="event-start-time-1">From</label>
-                <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${convertEventDateForEditForm(
-                  startDate
-                )}">
+                <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${convertEventDateForEditForm(startDate)}">
                 &mdash;
                 <label class="visually-hidden" for="event-end-time-1">To</label>
-                <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${convertEventDateForEditForm(
-                  endDate
-                )}">
+                <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${convertEventDateForEditForm(endDate)}">
               </div>
               <div class="event__field-group  event__field-group--price">
                 <label class="event__label" for="event-price-1">
@@ -152,13 +134,8 @@ const createEditFormTemplate = (event) => {
               </section>
               <section class="event__section  event__section--destination">
                 <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-                <p class="event__destination-description">${createDestinationDescriptionTemplate(
-                  DESTINATIONS,
-                  data.name
-                )}</p>
-                ${
-                  data.pictures ? createPicturesListTemplate(data.pictures) : ''
-                }
+                <p class="event__destination-description">${createDestinationDescriptionTemplate(DESTINATIONS,data.name)}</p>
+                ${data.pictures ? createPicturesListTemplate(data.pictures) : ''}
               </section>
             </section>
             </form>`;
